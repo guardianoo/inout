991bf79dc2ac77c45e0ce4fe9bfa3226eb9bc8c8
Levi Mendes <levispmendes@gmail.com>
2023-09-11 17:03:34 -0300

    v1.5

--- in.py
+++ in.py
@@ -1,18 +1,24 @@
 import os
 
-path = os.path.dirname(r"c:\Users\levis\OneDrive\Documents\Área de Trabalho\MBR Digital\# METALURGICA BUFALO\# FOTOS EM ALTA")
-
-path = 
-
 cwd = os.getcwd()
+path = os.path.abspath(cwd)
+
+file = __file__
+
+os.listdir('D:\AGENCIA BIT3D\#25 ARCIELO FITNESS\# BOLETOS')
+
+os.chdir('D:\AGENCIA BIT3D\#25 ARCIELO FITNESS\# BOLETOS')
+
+os.getcwd()
+
 full_list = os.listdir(path)
 files_list = [i.lower() for i in full_list if os.path.isfile(i) and '.py' not in i]
 
-types = list(set([i.split('.')[-1] for i in files_list]))
-for file_type in types:
-    os.mkdir(file_type)
+# types = list(set([i.split('.')[-1] for i in files_list]))
+# for file_type in types:
+#     os.mkdir(file_type)
 
-for file in files_list:
-    path_from = cwd + '/' + file
-    path_to = cwd + '/' + file.split('.')[-1] + '/' + file
-    os.replace(path_from, path_to)
+# for file in files_list:
+#     path_from = cwd + '/' + file
+#     path_to = cwd + '/' + file.split('.')[-1] + '/' + file
+#     os.replace(path_from, path_to)
